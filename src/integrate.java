Nesvilab/TMT-Integrator

commit 29f0b9371aa0e13c553e048924f565ff8c250416

add peptide start/end index to the peptide and site reports
note: still computing position in protein internally, not using psm.tsv's recorded position

src/integrate.java
@@ -836,6 +836,7 @@ public class integrate
             else if(groupBy==3 || groupBy == 5){ //multi-site and multi-mass
                 String pepStr = "";
                 String extPepStr = "";
+                String pepIndexStr = "";
                 for(String proteinID : ProtMap.keySet()){
                     List<String> pepLi = ProtMap.get(proteinID);
                     for(String pep: pepLi){
@@ -848,6 +849,7 @@ public class integrate
                         int pepIndex = (tmpProtSeq.indexOf(tmpep)>0)?tmpProtSeq.indexOf(tmpep):0;
                         int sIndex = pepIndex - 7;
                         int eIndex = pepIndex + pep.length();
+                        pepIndexStr += (pepIndex + 1) + "\t" + (eIndex + 1) + ";";
                         for(int i=sIndex; i<pepIndex; i++){
                             if(sIndex<0){
                                 extPepStr += "_";
@@ -870,18 +872,28 @@ public class integrate
                 }
                 pepStr = pepStr.substring(0,pepStr.length()-1);
                 extPepStr = extPepStr.substring(0,extPepStr.length()-1);
-                ggpStr += "\t"+proteinIDStr+"\t"+pepStr+"\t"+extPepStr;
+                pepIndexStr = pepIndexStr.substring(0,pepIndexStr.length()-1);
+                ggpStr += "\t"+proteinIDStr+"\t"+pepStr+"\t"+extPepStr + "\t" + pepIndexStr;
             }
             else{
                 String pepStr = "";
+                String pepIndexStr = "";
                 for(String protein : ProtMap.keySet()){
+                    String ProtSeq = param.fastaMap.get(param.phMap.get(param.ppMap.get(protein)));
+                    String tmpProtSeq = ProtSeq.replaceAll("[IL]","-");
                     List<String> pepLi = ProtMap.get(protein);
                     for(String pep : pepLi){
                         pepStr += pep + ";";
+
+                        String tmpep = pep.replaceAll("[IL]", "-").toUpperCase();
+                        int pepStart = (tmpProtSeq.indexOf(tmpep) > 0) ? tmpProtSeq.indexOf(tmpep) + 1 : 0;
+                        int pepEnd = pepStart + pep.length();
+                        pepIndexStr += pepStart + "\t" + pepEnd + ";";
                     }
                 }
                 pepStr = pepStr.substring(0,pepStr.length()-1);
-                ggpStr += "\t"+proteinIDStr+"\t"+pepStr;
+                pepIndexStr = pepIndexStr.substring(0,pepIndexStr.length()-1);
+                ggpStr += String.format("\t%s\t%s\t%s", proteinIDStr, pepStr, pepIndexStr);
             }
             groupkey = (ggpStr!="") ? (groupkey+"\t"+ggpStr+"\t"+MaxPepProb) : groupkey+"\t"+MaxPepProb;
             gAbnMap.put(groupkey, fAbnMap);
@@ -1162,10 +1174,10 @@ public class integrate
             wr.write("Index\tNumberPSM\tGene");
         }
         else if (groupBy==2){ //2: Peptide
-            wr.write("Index\tGene\tProteinID\tPeptide");
+            wr.write("Index\tGene\tProteinID\tPeptide\tStart\tEnd");
         }
         else if ((groupBy==3) || (groupBy==4) || (groupBy==5)){ //3. Site
-            wr.write("Index\tGene\tProteinID\tPeptide\tSequenceWindow");
+            wr.write("Index\tGene\tProteinID\tPeptide\tSequenceWindow\tStart\tEnd");
         }
         else if (groupBy==0){ //0. Gene
             wr.write("Index\tNumberPSM\tProteinID");

commit d17121fa03c53d1799828cc7899b1b5367eb3e19

fix bug introduced in 29f0b93 - add peptide start/end index to the peptide and site reports

src/integrate.java
@@ -838,6 +838,8 @@ public class integrate
                 String extPepStr = "";
                 String pepIndexStr = "";
                 for(String proteinID : ProtMap.keySet()){
+                    int lowestStart = (int) 1E6;
+                    int highestEnd = 0;
                     List<String> pepLi = ProtMap.get(proteinID);
                     for(String pep: pepLi){
                         pepStr+=pep+";";
@@ -849,7 +851,17 @@ public class integrate
                         int pepIndex = (tmpProtSeq.indexOf(tmpep)>0)?tmpProtSeq.indexOf(tmpep):0;
                         int sIndex = pepIndex - 7;
                         int eIndex = pepIndex + pep.length();
-                        pepIndexStr += (pepIndex + 1) + "\t" + (eIndex + 1) + ";";
+                        // only change the start/end string if a longer peptide is found
+                        if (pepIndex < lowestStart || eIndex > highestEnd) {
+                            pepIndexStr = (pepIndex + 1) + "\t" + (eIndex + 1);
+                            if (pepIndex < lowestStart) {
+                                lowestStart = pepIndex;
+                            }
+                            if (eIndex > highestEnd) {
+                                highestEnd = eIndex;
+                            }
+                        }
+
                         for(int i=sIndex; i<pepIndex; i++){
                             if(sIndex<0){
                                 extPepStr += "_";
@@ -872,7 +884,6 @@ public class integrate
                 }
                 pepStr = pepStr.substring(0,pepStr.length()-1);
                 extPepStr = extPepStr.substring(0,extPepStr.length()-1);
-                pepIndexStr = pepIndexStr.substring(0,pepIndexStr.length()-1);
                 ggpStr += "\t"+proteinIDStr+"\t"+pepStr+"\t"+extPepStr + "\t" + pepIndexStr;
             }
             else{
@@ -882,17 +893,27 @@ public class integrate
                     String ProtSeq = param.fastaMap.get(param.phMap.get(param.ppMap.get(protein)));
                     String tmpProtSeq = ProtSeq.replaceAll("[IL]","-");
                     List<String> pepLi = ProtMap.get(protein);
+                    int lowestStart = (int) 1E6;
+                    int highestEnd = 0;
                     for(String pep : pepLi){
                         pepStr += pep + ";";
 
                         String tmpep = pep.replaceAll("[IL]", "-").toUpperCase();
                         int pepStart = (tmpProtSeq.indexOf(tmpep) > 0) ? tmpProtSeq.indexOf(tmpep) + 1 : 0;
                         int pepEnd = pepStart + pep.length();
-                        pepIndexStr += pepStart + "\t" + pepEnd + ";";
+                        // only change the start/end string if a longer peptide is found
+                        if (pepStart < lowestStart || pepEnd > highestEnd) {
+                            pepIndexStr = (pepStart + 1) + "\t" + (pepEnd + 1);
+                            if (pepStart < lowestStart) {
+                                lowestStart = pepStart;
+                            }
+                            if (pepEnd > highestEnd) {
+                                highestEnd = pepEnd;
+                            }
+                        }
                     }
                 }
                 pepStr = pepStr.substring(0,pepStr.length()-1);
-                pepIndexStr = pepIndexStr.substring(0,pepIndexStr.length()-1);
                 ggpStr += String.format("\t%s\t%s\t%s", proteinIDStr, pepStr, pepIndexStr);
             }
             groupkey = (ggpStr!="") ? (groupkey+"\t"+ggpStr+"\t"+MaxPepProb) : groupkey+"\t"+MaxPepProb;