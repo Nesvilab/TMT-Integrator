3bbde205e15f749973f0318b7b3e02b6419d87e2
Fengchao <fcyucn@gmail.com>
2025-01-31 12:52:32 -0500

    Bump to 6.0.5

--- src/main/java/tmtintegrator/TMTIntegrator.java
+++ src/main/java/tmtintegrator/TMTIntegrator.java
@@ -14,7 +14,7 @@ import tmtintegrator.utils.ReportData;
 public class TMTIntegrator {
 
     private final static String APP_NAME = "TMT Integrator";
-    private final static String APP_VERSION = "6.0.4";
+    private final static String APP_VERSION = "6.0.5";
     private final Parameters param;
     private final ReportData reportData;
 
