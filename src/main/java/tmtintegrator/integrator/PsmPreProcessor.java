257b9c05fed7bc1ee597dddfbf280a7ae6c957aa
Fengchao <fcyucn@gmail.com>
2026-02-09 15:31:19 -0500

    Add license

--- src/main/java/tmtintegrator/integrator/PsmPreProcessor.java
+++ src/main/java/tmtintegrator/integrator/PsmPreProcessor.java
@@ -1,3 +1,17 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package tmtintegrator.integrator;
 
 import java.io.BufferedReader;
@@ -22,8 +36,6 @@ import tmtintegrator.utils.Utils;
 
 /**
  * Preprocess PSM files, check for missing values, create index for each PSM file
- *
- * @author rogerli on 05/2024
  */
 public class PsmPreProcessor {
 
